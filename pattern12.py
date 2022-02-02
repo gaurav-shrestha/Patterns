bcfade3c6a65748cabd0b17890d59f97330fc3ae
gaurav-shrestha <gvstha10@gmail.com>
2022-02-02 11:48:21 +0545

    Diamond pattern 2

---
+++ pattern12.py
@@ -0,0 +1,25 @@
+# Diamond Pattern 2
+'''
+
+    * 
+   * *
+  * * *
+ * * * *
+  * * *
+   * *
+    *
+
+'''
+n=int(input("Enter number of rows of upper half:"))
+for i in range(n-1):
+    for j in range(i,n):
+        print(end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+for i in range(n):
+    for j in range(i+1):
+        print(end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
