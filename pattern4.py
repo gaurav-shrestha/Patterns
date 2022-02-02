146030fdf61df5cb8ee2b9a71fb88c61a558219b
gaurav-shrestha <gvstha10@gmail.com>
2022-02-02 10:25:34 +0545

    Left Triangle

---
+++ pattern4.py
@@ -0,0 +1,16 @@
+# Left Triangle
+'''
+         *
+       * *
+     * * *
+   * * * *
+ * * * * *
+'''
+# In this pattern there is one decreasing triangle and one increasing triangle
+n=5
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
