aa29f96c5e7150c9eac211ad3557a02612671e9a
gaurav-shrestha <gvstha10@gmail.com>
2022-02-02 19:42:45 +0545

    Left Pascal's Triangle

---
+++ pattern14.py
@@ -0,0 +1,22 @@
+# Left Pascal's Triangle
+"""
+
+*          
+* *       
+* * *    
+* * * * 
+* * *   
+* *     
+*       
+
+"""
+n= int(input("Enter number of upper-half rows: "))
+
+for i in range(n-1):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+for i in range(n):
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
