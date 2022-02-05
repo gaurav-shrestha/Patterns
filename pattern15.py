5d99d51a5e012593df19af1fa7a2104299de4ae1
gaurav-shrestha <gvstha10@gmail.com>
2022-02-05 23:29:59 +0545

    Second approach for Right Pascal

--- pattern15.py
+++ pattern15.py
@@ -13,13 +13,13 @@
 n= int(input("Enter number of upper-half rows: "))
 
 for i in range(n-1):
-    for j in range(i,n-1):
+    for j in range(i,n):
         print(" ",end=" ")
     for j in range(i+1):
         print("*",end=" ")
     print()
 for i in range(n):
-    for j in range(i):
+    for j in range(i+1):
         print(" ",end=" ")
     for j in range(i,n):
         print("*",end=" ")
